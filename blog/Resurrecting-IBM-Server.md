58c3b23cf29986431ec71c3f589fc13e58ab3921
jacob <jacobmetz@proton.me>
2025-01-19 23:15:54 -0700

    implemented blog post handling

---
+++ blog/Resurrecting-IBM-Server.md
@@ -0,0 +1,14 @@
+---
+title: "Resurrecting an IBM Server: Building a FreeBSD Home Server in a Vintage x3200 M2"
+date: 2025-01-19
+excerpt: How I turned an old gaming computer and a obsolete 2 core server into a FreeBSD based do it all home server.
+image: "/images/IBM-server.jpg"
+---
+
+# Lorem Ipsom 
+
+Lorem ipsum odor amet, consectetuer adipiscing elit. Litora curabitur rutrum euismod quam sociosqu non a. Nisl condimentum risus purus viverra bibendum porttitor aliquet placerat fames. Nibh aenean eleifend scelerisque montes, non etiam eu enim. Nullam habitasse non lorem non platea lacus quam. Tempor placerat integer curabitur vivamus bibendum maecenas. Suscipit augue nunc vitae luctus laoreet. Pharetra consequat nullam natoque, aenean tincidunt sed.
+
+## Lorem Ipsom 
+
+Semper cursus sapien tristique magnis egestas donec curabitur urna. Lobortis efficitur orci nibh magna rutrum elementum ornare. Potenti suscipit pulvinar vehicula cras luctus a. Convallis nulla porta placerat rhoncus efficitur porta. Felis phasellus taciti aliquet curae, accumsan vel consequat. Sociosqu montes bibendum pellentesque consectetur mauris fermentum nisl. Vel penatibus dictum eu ante laoreet augue accumsan fringilla. Ridiculus blandit pellentesque non; tortor faucibus praesent tempor mattis. Ornare aliquet efficitur non laoreet ullamcorper. Suspendisse mattis et et sollicitudin per molestie elit donec.
--- server.js
+++ server.js
@@ -1,56 +1,75 @@
 // server.js
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const matter = require('gray-matter');
+const marked = require('marked');
+
 const app = express();
 
 // View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 // Serve static files from public directory
 app.use(express.static('public'));
 
-// Sample blog data (later you can move this to a separate data file)
-const blogPosts = [
-  {
-    title: "Getting Started with Modern Web Development",
-    slug: "getting-started",
-    date: "January 15, 2025",
-    excerpt: "An exploration of the latest tools and technologies that are shaping the future of web development...",
-    content: "Full blog post content here...",
-    image: "/api/placeholder/400/200"
-  },
-  // Add more posts as needed
-];
+// Blog post loading function
+function getBlogPosts() {
+    const postsDirectory = path.join(__dirname, 'blog');
+    try {
+        const filenames = fs.readdirSync(postsDirectory);
+        
+        const posts = filenames.map(filename => {
+            const filePath = path.join(postsDirectory, filename);
+            const fileContents = fs.readFileSync(filePath, 'utf8');
+            const { data, content } = matter(fileContents);
+            
+            return {
+                ...data,
+                slug: filename.replace('.md', ''),
+                content: marked.parse(content)
+            };
+        });
+
+        return posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+    } catch (error) {
+        console.error('Error loading blog posts:', error);
+        return [];
+    }
+}
+
+const blogPosts = getBlogPosts();
 
 // Routes
 app.get('/', (req, res) => {
-  res.render('index', {
-    name: 'Jacob Metz',
-    blogPosts: blogPosts.slice(0, 3) // Show only latest 3 posts on homepage
-  });
+    res.render('index', {
+        name: 'Jacob Metz',
+        blogPosts: blogPosts.slice(0, 3) // Show only latest 3 posts on homepage
+    });
 });
 
 // Blog routes
 app.get('/blog', (req, res) => {
-  res.render('blog/index', {
-    name: 'Jacob Metz',
-    blogPosts
-  });
+    res.render('blog/index', {
+        name: 'Jacob Metz',
+        blogPosts
+    });
 });
 
 app.get('/blog/:slug', (req, res) => {
-  const post = blogPosts.find(post => post.slug === req.params.slug);
-  if (!post) {
-    return res.status(404).render('404');
-  }
-  res.render('blog/post', {
-    name: 'Jacob Metz',
-    post
-  });
+    const post = blogPosts.find(post => post.slug === req.params.slug);
+    if (!post) {
+        return res.status(404).render('404');
+    }
+    res.render('blog/post', {
+        name: 'Jacob Metz',
+        post
+    });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
+console.log(blogPosts);
